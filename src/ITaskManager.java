989208e2d2e61ac7158ab80160db562c0ba2406b
SashaTyutyaev <sashatut2004@yandex.ru>
2023-10-18 17:09:23 +0300

    delete: удалить updateEpicStatus

--- src/ITaskManager.java
+++ src/ITaskManager.java
@@ -1,26 +1,45 @@
 import Tasks.*;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface ITaskManager {
-    ArrayList<Task> getTasks();
-    ArrayList<Subtask> getSubtasks();
-    ArrayList<Epic> getEpics();
+    HashMap<Integer, Task> getTasks();
+
+    HashMap<Integer, Subtask> getSubtasks();
+
+    HashMap<Integer, Epic> getEpics();
+
     ArrayList<Subtask> getEpicSubtasks(int epicId);
+
     Task getTask(int id);
+
     Subtask getSubtask(int id);
+
     Epic getEpic(int id);
+
     int addNewTask(Task task);
+
     int addNewEpic(Epic epic);
+
     Integer addNewSubtask(Subtask subtask);
+
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
+
     void deleteTask(int id);
+
     void deleteEpic(int id);
+
     void deleteSubtask(int id);
-    void updateEpicStatus(int epicId);
+
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
 
 }
